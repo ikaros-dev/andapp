85af91e7e458fedebafcea47042dd13b83196bbd
li-guohao <git@liguohao.cn>
2023-07-19 08:08:17 +0800

    use gsy video player

--- app/src/main/java/run/ikaros/app/and/SubjectDetailsActivity.java
+++ app/src/main/java/run/ikaros/app/and/SubjectDetailsActivity.java
@@ -1,7 +1,10 @@
 package run.ikaros.app.and;
 
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
@@ -10,43 +13,94 @@ import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.shuyu.gsyvideoplayer.GSYBaseActivityDetail;
+import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 
 import run.ikaros.app.and.constants.TmpConst;
 import run.ikaros.app.and.databinding.ActivitySubjectDetailsBinding;
 
-public class SubjectDetailsActivity extends AppCompatActivity {
+public class SubjectDetailsActivity extends GSYBaseActivityDetail<StandardGSYVideoPlayer> {
 
-    private ActivitySubjectDetailsBinding binding;
+    StandardGSYVideoPlayer detailPlayer;
+
+        private String url = "http://7xjmzj.com1.z0.glb.clouddn.com/20171026175005_JObCxCE2.mp4";
+//    private String url = "http://alvideo.ippzone.com/zyvd/98/90/b753-55fe-11e9-b0d8-00163e0c0248";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_details);
 
-        binding = ActivitySubjectDetailsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        detailPlayer = (StandardGSYVideoPlayer) findViewById(R.id.detail_player);
+        //增加title
+        detailPlayer.getTitleTextView().setVisibility(View.GONE);
+        detailPlayer.getBackButton().setVisibility(View.GONE);
 
-        final ImageView imageView = binding.imageView;
-        Uri imageUri = Uri.parse(TmpConst.Nas.Subject.COVER);
-        Glide.with(this)
-                .load(imageUri)
-                .into(imageView);
+        initVideoBuilderMode();
 
-        final VideoView videoView = binding.videoView;
-        videoView.setMediaController(new MediaController(this));
-        Uri videoUri = Uri.parse(TmpConst.Nas.Subject.VIDEO);
-        videoView.setVideoURI(videoUri);
-        videoView.start();
-        videoView.requestFocus();
-
-        final TextView titleTextView = binding.subjectTitle;
-        titleTextView.setText(TmpConst.Nas.Subject.TITLE);
-
-        final TextView titleCnTextView = binding.subjectTitleCn;
-        titleCnTextView.setText(TmpConst.Nas.Subject.TITLE_CN);
-
-        final TextView descTextView = binding.subjectDesc;
-        String desc = TmpConst.Nas.Subject.DESC;
-        descTextView.setText(desc);
+        //允许window 的内容可以上移到刘海屏状态栏
+        if (getWindow() != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(lp);
+        }
 
     }
+
+
+    @Override
+    public StandardGSYVideoPlayer getGSYVideoPlayer() {
+        return detailPlayer;
+    }
+
+    @Override
+    public GSYVideoOptionBuilder getGSYVideoOptionBuilder() {
+        //内置封面可参考SampleCoverVideo
+        ImageView imageView = new ImageView(this);
+        //loadCover(imageView, url);
+        return new GSYVideoOptionBuilder()
+                .setThumbImageView(imageView)
+                .setUrl(url)
+                .setCacheWithPlay(true)
+                .setVideoTitle("这里是一个竖直方向的视频")
+                .setIsTouchWiget(true)
+                //.setAutoFullWithSize(true)
+                .setRotateViewAuto(false)
+                .setLockLand(false)
+                .setShowFullAnimation(false)//打开动画
+                .setNeedLockFull(true)
+                .setSeekRatio(1);
+    }
+
+    @Override
+    public void clickForFullScreen() {
+
+    }
+
+
+    /**
+     * 是否启动旋转横屏，true表示启动
+     */
+    @Override
+    public boolean getDetailOrientationRotateAuto() {
+        return true;
+    }
+
+//    private void loadCover(ImageView imageView, String url) {
+//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        imageView.setImageResource(R.mipmap.xxx1);
+//        Glide.with(this.getApplicationContext())
+//                .setDefaultRequestOptions(
+//                        new RequestOptions()
+//                                .frame(3000000)
+//                                .centerCrop()
+//                                .error(R.mipmap.xxx2)
+//                                .placeholder(R.mipmap.xxx1))
+//                .load(url)
+//                .into(imageView);
+//    }
+
 }
